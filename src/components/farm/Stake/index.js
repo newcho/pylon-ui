a1d0af923d4b7dbacb5b78ceca1c01cf03f5a4ee
ricardogoncalvescho <ricardogoncalves0725@gmail.com>
2020-12-20 02:56:56 -0500

    update percent

--- src/components/farm/Stake/index.js
+++ src/components/farm/Stake/index.js
@@ -144,7 +144,7 @@ export default function ({
           </div>
           <div className="stake-header-section">
             <TextBlock
-              title="38%"
+              title="52%"
               content={type == "FDI" ? "APY" : "Mining Seed APY"}
               colorTitle="#fd8300"
               colorContent="#00b9ea"
